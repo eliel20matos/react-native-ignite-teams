52f9d6995d28ee67366c3b88ff3e0928dd55a1db
eliellima00 <eliel.lima@attosementes.com.br>
2022-11-03 19:18:22 -0400

    feat: render component with flat list

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,13 +2,21 @@ import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
 import { Container } from "./styles";
+import { useState } from "react";
+import { FlatList } from "react-native";
 
 export function Groups() {
+  const [groups, setGroups] = useState(["Galera da ATTO", "Galera da FATEC"]);
+
   return (
     <Container>
       <Header showBackButton />
       <Highlight title="Turmas" subtitle="Jogue com sua turma" />
-      <GroupCard title="Galera do ignite"/>
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   );
 }
