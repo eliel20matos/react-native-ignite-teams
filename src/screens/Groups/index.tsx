261b53aa1e5983252f17ed573fe231bbb778a882
eliellima00 <eliel.lima@attosementes.com.br>
2022-11-03 19:27:21 -0400

    feat: ListEmpty component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,9 +4,10 @@ import { GroupCard } from "@components/GroupCard";
 import { Container } from "./styles";
 import { useState } from "react";
 import { FlatList } from "react-native";
+import { ListEmpty } from "@components/ListEmpty";
 
 export function Groups() {
-  const [groups, setGroups] = useState(["Galera da ATTO", "Galera da FATEC"]);
+  const [groups, setGroups] = useState([]);
 
   return (
     <Container>
@@ -16,6 +17,10 @@ export function Groups() {
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
       />
     </Container>
   );
