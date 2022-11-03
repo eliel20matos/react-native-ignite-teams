bc16d01ef0d3ccd6bfe526db6d45544a3f8a8e80
eliellima00 <eliel.lima@attosementes.com.br>
2022-11-03 09:04:06 -0400

    feat: GroupCard component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,14 @@
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
+import { GroupCard } from "@components/GroupCard";
 import { Container } from "./styles";
 
 export function Groups() {
   return (
     <Container>
       <Header showBackButton />
+      <Highlight title="Turmas" subtitle="Jogue com sua turma" />
+      <GroupCard title="Galera do ignite"/>
     </Container>
   );
 }
