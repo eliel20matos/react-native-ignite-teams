af4b42262e5c6b5fd3b44f32c963213b032df77c
eliellima00 <eliel.lima@attosementes.com.br>
2022-11-03 08:12:33 -0400

    feat: initial setup and page

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import { BackIcon, Container, Logo, BackButton } from "./styles";
+import logoImg from "@assets/logo.png";
+
+type Props = {
+  showBackButton?: boolean;
+};
+
+export function Header({ showBackButton = false }: Props) {
+  return (
+    <Container>
+      {showBackButton ? (
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      ) : null}
+      <Logo source={logoImg} />
+    </Container>
+  );
+}
---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { Container } from "./styles";
+
+export function Groups() {
+  return (
+    <Container>
+      <Header showBackButton />
+    </Container>
+  );
+}
