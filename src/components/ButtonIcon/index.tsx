1589da38c58f03d59daaa6c64e9337647c8995fc
eliel20matos <eliellimamatos@gmail.com>
2022-11-04 08:57:47 -0400

    feat: icon with variations and form

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,17 @@
+import { TouchableOpacityProps } from "react-native";
+
+import { Container, ButtonIconTypeStyleProps, Icon } from "./styles";
+import { MaterialIcons } from "@expo/vector-icons";
+
+type Props = TouchableOpacityProps & {
+  icon: keyof typeof MaterialIcons.glyphMap;
+  type?: ButtonIconTypeStyleProps;
+};
+
+export function ButtonIcon({ type = "PRIMARY", icon, ...rest }: Props) {
+  return (
+    <Container {...rest}>
+      <Icon name={icon} type={type} />
+    </Container>
+  );
+}
