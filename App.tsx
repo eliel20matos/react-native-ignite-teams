ce87b73cb0a0c035dd7996c595c7898805c743bb
eliel20matos <eliellimamatos@gmail.com>
2022-11-07 20:21:13 -0400

    feat: add react navigation

--- App.tsx
+++ App.tsx
@@ -5,11 +5,9 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
-import { Groups } from "@screens/Groups";
 import theme from "@theme/index";
 import { Loading } from "@components/Loading";
-import { NewGroup } from "@screens/NewGroup";
-import { Players } from "@screens/Players";
+import { Routes } from "@routes/index";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -21,7 +19,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Players /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   );
 }
